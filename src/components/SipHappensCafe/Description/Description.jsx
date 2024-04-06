21ef1dc6ec320f002451ef036def3b6a9cb27f7b
Wolf <andresistuk@gmail.com>
2024-04-06 21:37:16 +0300

    add margin header

--- src/components/SipHappensCafe/Description/Description.jsx
+++ src/components/SipHappensCafe/Description/Description.jsx
@@ -3,7 +3,7 @@ import css from './Description.module.css';
 export default function Description() {
  return (
     <div className={css.description}>
-    <h1>Sip Happens Café</h1>
+    <h1 className={css.header}>Sip Happens Café</h1>
     <p>Please leave your feedback about our service by selecting one of the options below.</p>
     </div>
  );
